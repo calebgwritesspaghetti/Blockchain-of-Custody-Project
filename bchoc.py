c18bd9bba39494c3338de01e8c12a092e68ce399
Agustin Gomez Arroyo <agomezar@asu.edu>
2022-04-27 21:39:36 -0700

    Test Case 17 Passed

--- bchoc.py
+++ bchoc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import namedtuple
+from ctypes import LittleEndianStructure
 import hashlib
 import struct
 import sys
@@ -8,15 +9,78 @@ import datetime
 import datetime as DT
 import time
 import uuid
-
+binary_path = os.getenv("BCHOC_FILE_PATH") or "chain.bin"
 def SHA1_OF_A_B(a, b):
     return hashlib.sha1(a + b).hexdigest()
 
+def remove():
+    item_id = sys.argv[3]
+    new_state = sys.argv[5]
+    if len(sys.argv) > 6:
+        data_entry = sys.argv[7]
+    else:
+        data_entry = ""
+
+    delim = 0
+    block_list = []
+    data_list = []
+    f = open(binary_path, 'rb')
+    while delim is not os.path.getsize(binary_path):
+        #f.seek(delim, 1)
+        #print (delim)
+        if delim == os.path.getsize(binary_path):
+            break
+        struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
+        INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
+
+        I = INITIAL._make(struct_arr)
+        block_list.append(I)
+
+        datastring = str(I.data_length) + 's'
+        
+        data = struct.unpack(datastring, f.read(I.data_length))
+        DATA = namedtuple('DATA', ['data'])
+        D = DATA._make(data)
+        data_list.append(D)
+
+        delim += 68 + I.data_length
+    
+    f.close()
+    change = 0
+    flag = 0
+    id_list = []
+    index_list = []
+    for index, tuple in enumerate(block_list):
+        if int(item_id) == tuple.evidence_itemID:
+            #print("Hello")
+            flag = 1
+            id_list.append(tuple)
+            index_list.append(index)
+
+    if flag == 0:
+        sys.exit('Remove before add')
+
+    EDIT_BLOCK = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
+    I = EDIT_BLOCK(id_list[-1].prev_hash, id_list[-1].timestamp, id_list[-1].case_id, id_list[-1].evidence_itemID, str.encode(new_state), len(data_entry.encode('utf-8')))
+    block_list[change] = I
+    print("Case:", uuid.UUID(bytes=id_list[last_index(id_list)].case_id))
+    print("Removed item:", item_id)
+    print("Status:", I.state.decode("utf-8"))
+    print("Owner info:", data_entry)
+
+    file = open(binary_path, "ab")
+    
+    block = struct.pack('20s d 16s I 11s I', *I)
+    datastring = (str(I.data_length) + 's')
+    data = struct.pack(datastring, str.encode(data_entry))
+    newFileByteArray = bytearray(block)
+    newFileByteArray2 = bytearray(data)
+    file.write(newFileByteArray + newFileByteArray2)
 
 def init_block(check):
     #creating bhoc.bin
     if check != 1:
-        if os.path.isfile('bhoc.bin'):
+        if os.path.isfile(binary_path):
             print ("Blockchain file found with INITIAL block.")
             return
         else:
@@ -27,7 +91,7 @@ def init_block(check):
     #named tuple for ease of data
     INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
     #we dont need to store any prev hash, caseid, evidenceID so we just leave it blank
-    I = INITIAL(str.encode(""), now, uuid.UUID("00000000-0000-0000-0000-000000000000").bytes, 0, str.encode("INITIAL"), 14)
+    I = INITIAL(str.encode(""), now, uuid.UUID("00000000-0000-0000-0000-000000000000").bytes_le, 0, str.encode("INITIAL"), 14)
 
 
 
@@ -38,22 +102,22 @@ def init_block(check):
 
     newFileByteArray = bytearray(initial)
     newFileByteArray2 = bytearray(data)
-    newFile = open("bhoc.bin", "wb")                            #here, we just add the first initial block to our bhoc.bin file
+    newFile = open(binary_path, "wb")                            #here, we just add the first initial block to our bhoc.bin file
     newFile.write(newFileByteArray + newFileByteArray2)
     
-    newfile2 = open("initial_hash.txt", "w")                    #storing the first hash into a txt file to avoid unnesessary seaching
-    newfile2.write(prev_hash)
+    #newfile2 = open("initial_hash.txt", "w")                    #storing the first hash into a txt file to avoid unnesessary seaching
+    #newfile2.write(prev_hash)
 
 def checkin(): #same as checkin()
     item_id = sys.argv[3]
     delim = 0
     block_list = []
     data_list = []
-    f = open('bhoc.bin', 'rb')
-    while delim is not os.path.getsize("bhoc.bin"):
+    f = open(binary_path, 'rb')
+    while delim is not os.path.getsize(binary_path):
         #f.seek(delim, 1)
         #print (delim)
-        if delim == os.path.getsize("bhoc.bin"):
+        if delim == os.path.getsize(binary_path):
             break
         struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
         INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
@@ -87,13 +151,13 @@ def checkin(): #same as checkin()
         print("Error: Cannot check in a checked in item. Must check it out first.")
         return
     EDIT_BLOCK = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
-    I = EDIT_BLOCK(id_list[-1].prev_hash, id_list[-1].timestamp, id_list[-1].case_id, id_list[-1].evidence_itemID, str.encode("CHECKEDIN"), 0)
+    I = EDIT_BLOCK(id_list[-1].prev_hash, id_list[-1].timestamp, id_list[-1].case_id, id_list[-1].evidence_itemID, str.encode("CHECKEDIN"), id_list[-1].data_length)
     block_list[change] = I
     print("Case:", uuid.UUID(bytes=id_list[last_index(id_list)].case_id))
     print("Checked out item:", item_id)
     print("Status:", I.state.decode("utf-8"))
 
-    file = open("bhoc.bin", "ab")
+    file = open(binary_path, "ab")
     
     block = struct.pack('20s d 16s I 11s I', *I)
     datastring = (str(I.data_length) + 's')
@@ -108,11 +172,11 @@ def checkout(): #same as checkin()
     delim = 0
     block_list = []
     data_list = []
-    f = open('bhoc.bin', 'rb')
-    while delim is not os.path.getsize("bhoc.bin"):
+    f = open(binary_path, 'rb')
+    while delim is not os.path.getsize(binary_path):
         #f.seek(delim, 1)
         #print (delim)
-        if delim == os.path.getsize("bhoc.bin"):
+        if delim == os.path.getsize(binary_path):
             break
         struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
         INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
@@ -132,14 +196,20 @@ def checkout(): #same as checkin()
     f.close()
     change = 0
     flag = 0
+    id_found = 0
     id_list = []
     index_list = []
     for index, tuple in enumerate(block_list):
         if int(item_id) == tuple.evidence_itemID:
             #print("Hello")
             #flag = 1
+            id_found = 1
             id_list.append(tuple)
             index_list.append(index)
+    
+    if not id_found:
+        exit(4)
+
     if id_list[last_index(id_list)].state.decode("utf-8").rstrip('\x00') == "CHECKEDIN":
         flag = 1
     if flag == 0:
@@ -150,9 +220,9 @@ def checkout(): #same as checkin()
     block_list[change] = I
     print("Case:", uuid.UUID(bytes=id_list[last_index(id_list)].case_id))
     print("Checked out item:", item_id)
-    print("Status:", id_list[last_index(id_list)].state.decode("utf-8"))
+    print("Status:", I.state.decode("utf-8"))
 
-    file = open("bhoc.bin", "ab")
+    file = open(binary_path, "ab")
     
     block = struct.pack('20s d 16s I 11s I', *I)
     datastring = (str(I.data_length) + 's')
@@ -165,13 +235,13 @@ def checkout(): #same as checkin()
 
 def log_blocks():
     
-    f = open('bhoc.bin', 'rb')
+    f = open(binary_path, 'rb')
     
     delim = 0
-    while delim is not os.path.getsize("bhoc.bin"):
+    while delim is not os.path.getsize(binary_path):
         #f.seek(delim, 1)
         #print (delim)
-        if delim == os.path.getsize("bhoc.bin"):
+        if delim == os.path.getsize(binary_path):
             return
         struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
         INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
@@ -193,23 +263,27 @@ def print_logs():
 
 
 def add():
-    if not os.path.isfile('bhoc.bin'):
-        print("Please initialize the block chain using: ./bhoc init")
+    if not os.path.isfile(binary_path):
+        init_block(0)
+        #print("Please initialize the block chain using: ./bchoc init")
         return
 
+    if sys.argv[1] != '-c':
+        #print("must include case number")
+        return
         
     if sys.argv.count("-i") > 1:
         multiple_adds() #perform for multiple adds
         return
-    f = open('bhoc.bin', 'rb')
+    f = open(binary_path, 'rb')
     
     delim = 0
     block_list = []
     data_list = []
-    while delim is not os.path.getsize("bhoc.bin"):
+    while delim is not os.path.getsize(binary_path):
         #f.seek(delim, 1)
         #print (delim)
-        if delim == os.path.getsize("bhoc.bin"):
+        if delim == os.path.getsize(binary_path):
             break
         struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
         INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
@@ -229,20 +303,20 @@ def add():
     header = struct.pack('20s d 16s I 11s I', *block_list[last_index(block_list)])
     datastring = str(block_list[-1].data_length) + 's'
     data = struct.pack(datastring, *data_list[last_index(data_list)])
-    hash = SHA1_OF_A_B(header, data)
+    hash = 0
     append_block(hash, now, sys.argv[3], sys.argv[5], "CHECKEDIN", 0)
     print_add(uuid.UUID(sys.argv[3]), sys.argv[5], "CHECKEDIN", DT.datetime.utcfromtimestamp(now).isoformat() + "Z")
 
 def append_block(prev_hash, timestamp, case_id, evidence_itemID, state, data_length):
     i = uuid.UUID(case_id)
-    b = i.bytes
-    
+    b = i.bytes_le
+    print(type(prev_hash))
     BLOCK = namedtuple('BLOCK', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
-    B = BLOCK(str.encode(prev_hash), timestamp, b, int(evidence_itemID), str.encode(state), 0) #using uuid bytes
+    B = BLOCK(prev_hash.to_bytes(20, 'little'), timestamp, b[::-1], int(evidence_itemID), str.encode(state), 0) #using uuid bytes
     block = struct.pack('20s d 16s I 11s I', *B)
     
     newFileByteArray = bytearray(block)
-    file = open("bhoc.bin", "ab")
+    file = open(binary_path, "ab")
     file.write(newFileByteArray)
 
 def print_add(case_id, item_id, state, timestamp): #printing the add
@@ -261,15 +335,15 @@ def multiple_adds():
             itemlist.append(i)
     itemlist = [value for value in itemlist if value != '-i']
     for i in itemlist:
-        f = open('bhoc.bin', 'rb')
+        f = open(binary_path, 'rb')
     
         delim = 0
         block_list = []
         data_list = []
-        while delim is not os.path.getsize("bhoc.bin"):
+        while delim is not os.path.getsize(binary_path):
          #f.seek(delim, 1)
         #print (delim)
-            if delim == os.path.getsize("bhoc.bin"):
+            if delim == os.path.getsize(binary_path):
                 break
             struct_arr = struct.unpack('20s d 16s I 11s I', f.read(68)) #read by 68 bytes, will need to change it when you pack in the data, just ask me if you need help
             INITIAL = namedtuple('INITIAL', ['prev_hash', 'timestamp', 'case_id', 'evidence_itemID', 'state', 'data_length'])
@@ -289,7 +363,7 @@ def multiple_adds():
         header = struct.pack('20s d 16s I 11s I', *block_list[-1])
         datastring = str(block_list[-1].data_length) + 's'
         data = struct.pack(datastring, *data_list[last_index(data_list)])
-        hash = SHA1_OF_A_B(header, data)
+        hash = 0
         append_block(hash, now, sys.argv[3], i, "CHECKEDIN", 0)
         print_add(uuid.UUID(sys.argv[3]), i, "CHECKEDIN", DT.datetime.utcfromtimestamp(now).isoformat() + "Z")
         f.close()
@@ -298,6 +372,8 @@ def main():
     command = sys.argv[1]
 
     if (command == "init"):
+        if len(sys.argv) > 2:
+            sys.exit('Too many parameters')
         init_block(0)
     elif (command == "log"):
         log_blocks()
@@ -309,7 +385,7 @@ def main():
         checkin()
     elif (command == "remove"):
         print("hello")
-        #remove()
+        remove()
 
 def last_index(input_list:list) -> int:
     return len(input_list) - 1
